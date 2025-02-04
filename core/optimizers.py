a60a9fff0def05b994abb31ce163d494444332ab
Katerina Fajmanova <k.moudra@gmail.com>
2025-02-04 23:12:31 +0100

    repaired optimizers when more are set as true at the same time.

--- core/optimizers.py
+++ core/optimizers.py
@@ -8,34 +8,27 @@ import streamlit as st
 import numpy as np # linear algebra
 import tensorflow as tf
 
-def run_optimizer(optimizer,func, x_init, y_init, lr=0.001, max_iters=1000):
+def run_optimizer(optimizer,func, x_init, y_init, max_iters=1000):
     x = tf.Variable(tf.constant(x_init))  # Starting value for x
     y = tf.Variable(tf.constant(y_init))  # Starting value for y
-    
     path = [(x.numpy(), y.numpy(), func(x, y).numpy())]
-
     # Perform optimization (minimizing the function f(x, y))
     for step in range(max_iters):
         with tf.GradientTape() as tape:
             # Record the function's output
             loss = func(x, y)
-        
         # Compute the gradients of loss with respect to x and y
         grads = tape.gradient(loss, [x, y])
-
         if grads[0] is None or grads[1] is None:
             print("No gradients provided for x or y")
             break
-        
         # Apply the gradients to update x and y (gradient descent step)
         optimizer.apply_gradients(zip(grads, [x, y]))
-
         # Track progress
         path.append((x.numpy(), y.numpy(), loss.numpy()))
-        
     return path
         
-def run_all_optimizers(selected_optimizers,function, x_init, y_init, lr, max_iters):
+def run_all_optimizers(selected_optimizers,function, x_init, y_init, max_iters):
     def func_py(x, y):
         return eval(function)
     # Store the loss history for each optimizer
@@ -43,7 +36,6 @@ def run_all_optimizers(selected_optimizers,function, x_init, y_init, lr, max_ite
     # Loop through each optimizer and run the optimization
     for optimizer_name, optimizer_solution in selected_optimizers.items():
         #print(f"Running {optimizer_name} optimizer...")
-        loss_history = run_optimizer(optimizer_solution, func_py, x_init, y_init, lr, max_iters)
+        loss_history = run_optimizer(optimizer_solution, func_py, x_init, y_init, max_iters)
         optimizer_results[optimizer_name]  = loss_history
-    
     return optimizer_results
--- steps/handle_step_optimizers_params.py
+++ steps/handle_step_optimizers_params.py
@@ -6,10 +6,6 @@ from config import settings
 
 def handle_step_optimizers_params(optimizer_descriptions, set_step):
     st.write("SGD proceeded to find the minimum. Now its your turn! Tune hyperparameters to get better results. Or have fun fooling SGD.")
-    
-    #plot_container2=st.container()     
-                
-   #st.write("Select TensorFlow Optimizers")
 
     # Function to load the active optimizers (enabled/disabled) from the JSON file
     def load_active_optimizers(file_path="active_optimizers.json"):
@@ -19,7 +15,6 @@ def handle_step_optimizers_params(optimizer_descriptions, set_step):
     # Function to configure optimizers based on the active status and settings
     def configure_optimizers(active_optimizers, optimizer_params=None):
         optimizers_dict = {}
-
         # Loop through each optimizer and check if it's active
         for optimizer_name, is_active in active_optimizers.items():
             if is_active:
@@ -28,13 +23,10 @@ def handle_step_optimizers_params(optimizer_descriptions, set_step):
                     # Override the optimizer config with user-defined parameters if provided
                     if optimizer_params and optimizer_name in optimizer_params:
                         optimizer_config.update(optimizer_params[optimizer_name])
-                    
                     # Dynamically get the optimizer class
                     optimizer_class = getattr(tf.optimizers, optimizer_name)
-
                     # Initialize the optimizer with all parameters from the config
                     optimizers_dict[optimizer_name] = optimizer_class(**optimizer_config)
-
         return optimizers_dict
 
     # Load the active optimizers from the JSON file
@@ -42,19 +34,14 @@ def handle_step_optimizers_params(optimizer_descriptions, set_step):
 
     pos_max_iters = [100, 1000, 10000]
     st.session_state.max_iters = st.select_slider("Max Iterations (maximum number of optimizer steps)", options=pos_max_iters, value=1000)
-
+    optimizer_params = {}
     optimizers_sel = {}
+    st.session_state.pos_learning_rates = [0.0001, 0.001, 0.01]
+
     for optimizer_name, is_active in active_optimizers.items():
         if is_active:
             optimizers_sel[optimizer_name] = st.toggle(f"Minimize me. ({optimizer_name})")
             # Get the optimizer description from the dictionary
-            st.session_state.max_iters = 1000
-            optimizer_params = {}
-
-
-
-            st.session_state.pos_learning_rates = [0.0001, 0.001, 0.01]
-
             # Collect custom parameters for each optimizer
             optimizer_info = optimizer_descriptions.get(optimizer_name)
             if optimizer_info:
@@ -67,7 +54,6 @@ def handle_step_optimizers_params(optimizer_descriptions, set_step):
 
                     st.session_state.learning_rate = 0.001
     
-   
                     optimizer_params[optimizer_name] = {
                         "learning_rate": st.select_slider("Learning Rate (size of optimizer step)", options=st.session_state.pos_learning_rates, value=0.001),
                         "momentum": st.slider("Momentum", min_value=0.0, max_value=1.0, value=0.9),
@@ -86,19 +72,19 @@ def handle_step_optimizers_params(optimizer_descriptions, set_step):
                     
                     optimizer_params[optimizer_name] = {
                         "learning_rate": st.select_slider("Learning Rate", options=st.session_state.pos_learning_rates, value=0.001),
+                        "beta_1": st.slider("Beta 1", min_value=0.5, max_value=0.99, value=0.9),
+                        "beta_2": st.slider("Beta 2", min_value=0.9, max_value=0.99, value=0.999)
                     }
 
     for optimizer_name, is_selected in optimizers_sel.items():
         active_optimizers[optimizer_name] = is_selected
 
-
     # Call configure_optimizers to get the optimizers with updated parameters
     st.session_state.optimizers_dict = configure_optimizers(optimizers_sel, optimizer_params)
 
-    optimizer_results_for_plot = core.run_all_optimizers(st.session_state.optimizers_dict,st.session_state.equation,float(st.session_state.x_init), float(st.session_state.y_init),lr=0.01,max_iters = 100) 
+    optimizer_results_for_plot = core.run_all_optimizers(st.session_state.optimizers_dict,st.session_state.equation,float(st.session_state.x_init), float(st.session_state.y_init),max_iters = st.session_state.max_iters) 
    
     with st.session_state.plot_container:
-        #figure = core.plot_path_history(st.session_state.func, optimizer_results_for_plot, st.session_state.equation, st.session_state.final_min_x_max_x, st.session_state.final_min_y_max_y) 
         figure = core.plot_function_with_start_point_and_history(st.session_state.func,st.session_state.equation,st.session_state.x_init,st.session_state.y_init, st.session_state.final_min_x_max_x, st.session_state.final_min_y_max_y, st.session_state.global_minima_f,st.session_state.local_minima_f,optimizer_results_for_plot)  
         st.pyplot(figure)
 
